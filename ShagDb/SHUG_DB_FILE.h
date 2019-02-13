a7261d730bf976493e36b1ed06c2b49020e2e55f
KuznetsovAlexandr <45228899+KuznetsovAlexandr@users.noreply.github.com>
2019-02-13 21:07:21 +0300

    Update SHUG_DB_FILE.h

--- ShagDb/SHUG_DB_FILE.h
+++ ShagDb/SHUG_DB_FILE.h
@@ -139,113 +139,113 @@ void Import_student_data_base(Student *stud)
 	{
 		string buffer;
 
-		getline(file_db, buffer);  // берем строку из файла file_db и помещаем в переменную баффер
+		getline(file_db, buffer);  // Р±РµСЂРµРј СЃС‚СЂРѕРєСѓ РёР· С„Р°Р№Р»Р° file_db Рё РїРѕРјРµС‰Р°РµРј РІ РїРµСЂРµРјРµРЅРЅСѓСЋ Р±Р°С„С„РµСЂ
 
 		int pos = 0;
-		pos = buffer.find(";"); // в строке buffer командой find ищем сколько символов до ;  и помещаем в переменную pos
+		pos = buffer.find(";"); // РІ СЃС‚СЂРѕРєРµ buffer РєРѕРјР°РЅРґРѕР№ find РёС‰РµРј СЃРєРѕР»СЊРєРѕ СЃРёРјРІРѕР»РѕРІ РґРѕ ;  Рё РїРѕРјРµС‰Р°РµРј РІ РїРµСЂРµРјРµРЅРЅСѓСЋ pos
 
 		string sub_str;
-		sub_str = buffer.substr(0, pos); // берем  из строки buffer командой substr код от 0 символа до символа равного pos и помещаем в переменную строку sub_str
-		cout << "Первый кусок кода " << sub_str << endl;
-		cout << "Позиция " << pos << endl;
+		sub_str = buffer.substr(0, pos); // Р±РµСЂРµРј  РёР· СЃС‚СЂРѕРєРё buffer РєРѕРјР°РЅРґРѕР№ substr РєРѕРґ РѕС‚ 0 СЃРёРјРІРѕР»Р° РґРѕ СЃРёРјРІРѕР»Р° СЂР°РІРЅРѕРіРѕ pos Рё РїРѕРјРµС‰Р°РµРј РІ РїРµСЂРµРјРµРЅРЅСѓСЋ СЃС‚СЂРѕРєСѓ sub_str
+		cout << "РџРµСЂРІС‹Р№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str << endl;
+		cout << "РџРѕР·РёС†РёСЏ " << pos << endl;
 
 
-		int id = atoi(sub_str.c_str());// atoi переводит строку в int 
+		int id = atoi(sub_str.c_str());// atoi РїРµСЂРµРІРѕРґРёС‚ СЃС‚СЂРѕРєСѓ РІ int 
 		stud->id = id;
 
 
 
 		string buffer2;
-		cout << "строка баффер " << buffer << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ " << buffer << endl;
 		pos = 0;
-		buffer2 = buffer.substr(pos + 2);               //    берем из большой строки 
-		cout << "строка баффер 2 " << buffer2 << endl;
+		buffer2 = buffer.substr(pos + 2);               //    Р±РµСЂРµРј РёР· Р±РѕР»СЊС€РѕР№ СЃС‚СЂРѕРєРё 
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 2 " << buffer2 << endl;
 		pos = buffer2.find(";");
 		string sub_str1;
 		sub_str1 = buffer2.substr(0, pos);
-		cout << "Второй кусок кода " << sub_str1 << endl;
+		cout << "Р’С‚РѕСЂРѕР№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str1 << endl;
 
 
 		stud->people.family = sub_str1;
 
 		string buffer3;
 		buffer3 = buffer2.substr(pos + 1);
-		cout << "строка баффер 3 " << buffer3 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 3 " << buffer3 << endl;
 		pos = 0;
 		pos = buffer3.find(";");
 		string sub_str2;
 		sub_str2 = buffer3.substr(0, pos);
-		cout << "Третий  кусок кода " << sub_str2 << endl;
+		cout << "РўСЂРµС‚РёР№  РєСѓСЃРѕРє РєРѕРґР° " << sub_str2 << endl;
 
-		double mid = atof(sub_str2.c_str());  // atof переводит строку в double
+		double mid = atof(sub_str2.c_str());  // atof РїРµСЂРµРІРѕРґРёС‚ СЃС‚СЂРѕРєСѓ РІ double
 		double mid(float);
 		stud->middle_ac = mid;
 
 		string buffer4;
 		buffer4 = buffer3.substr(pos + 1);
-		cout << "строка баффер 4 " << buffer4 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 4 " << buffer4 << endl;
 		pos = 0;
 		pos = buffer4.find(";");
 		string sub_str3;
 		sub_str3 = buffer4.substr(0, pos);
-		cout << "Четвертый кусок кода " << sub_str3 << endl;
+		cout << "Р§РµС‚РІРµСЂС‚С‹Р№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str3 << endl;
 
 		stud->facult.name = sub_str3;
 
 
 		string buffer5;
 		buffer5 = buffer4.substr(pos + 1);
-		cout << "строка баффер 5 " << buffer5 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 5 " << buffer5 << endl;
 		pos = 0;
 		pos = buffer5.find(";");
 		string sub_str4;
 		sub_str4 = buffer5.substr(0, pos);
-		cout << "Пятый кусок кода " << sub_str4 << endl;
+		cout << "РџСЏС‚С‹Р№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str4 << endl;
 
 		stud->facult.group = sub_str4;
 
 		string buffer6;
 		buffer6 = buffer5.substr(pos + 1);
-		cout << "строка баффер 6 " << buffer6 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 6 " << buffer6 << endl;
 		pos = 0;
 		pos = buffer6.find(";");
 		string sub_str5;
 		sub_str5 = buffer6.substr(0, pos);
-		cout << "Шестой кусок кода " << sub_str5 << endl;
+		cout << "РЁРµСЃС‚РѕР№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str5 << endl;
 
 		stud->people.name = sub_str5;
 
 
 		string buffer7;
 		buffer7 = buffer6.substr(pos + 1);
-		cout << "строка баффер 7 " << buffer7 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 7 " << buffer7 << endl;
 		pos = 0;
 		pos = buffer7.find(";");
 		string sub_str6;
 		sub_str6 = buffer7.substr(0, pos);
-		cout << "Седьмой кусок кода " << sub_str6 << endl;
+		cout << "РЎРµРґСЊРјРѕР№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str6 << endl;
 
 		stud->people.contact.address = sub_str6;
 
 		string buffer8;
 		buffer8 = buffer7.substr(pos + 1);
-		cout << "строка баффер 8 " << buffer8 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 8 " << buffer8 << endl;
 		pos = 0;
 		pos = buffer8.find(";");
 		string sub_str7;
 		sub_str7 = buffer8.substr(0, pos);
-		cout << "Восьмой кусок кода " << sub_str7 << endl;
+		cout << "Р’РѕСЃСЊРјРѕР№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str7 << endl;
 
 		int tel = atoi(sub_str7.c_str());
 		stud->people.contact.tel = tel;
 		string buffer9;
 		buffer9 = buffer8.substr(pos + 1);
-		cout << "строка баффер 9 " << buffer9 << endl;
+		cout << "СЃС‚СЂРѕРєР° Р±Р°С„С„РµСЂ 9 " << buffer9 << endl;
 		pos = 0;
 		pos = buffer9.find(";");
 		string sub_str8;
 		sub_str8 = buffer9.substr(0, pos);
-		cout << "Восьмой кусок кода " << sub_str8 << endl;
+		cout << "Р’РѕСЃСЊРјРѕР№ РєСѓСЃРѕРє РєРѕРґР° " << sub_str8 << endl;
 
 		int act = atoi(sub_str8.c_str());
 		stud->facult.actual = act;
@@ -258,11 +258,11 @@ void Import_student_data_base(Student *stud)
 
 
 /*
-Student_Shag_Visit.txt - посещаемость
-Student_Shag_AcPerf.txt - тек оценка
-Student_Shag_HomeWork.txt - оценка за д.з.
-Student_Shag_Test.txt  - оценка за тесты
-Student_Shag _Lab.txt - оценка за лабы
+Student_Shag_Visit.txt - РїРѕСЃРµС‰Р°РµРјРѕСЃС‚СЊ
+Student_Shag_AcPerf.txt - С‚РµРє РѕС†РµРЅРєР°
+Student_Shag_HomeWork.txt - РѕС†РµРЅРєР° Р·Р° Рґ.Р·.
+Student_Shag_Test.txt  - РѕС†РµРЅРєР° Р·Р° С‚РµСЃС‚С‹
+Student_Shag _Lab.txt - РѕС†РµРЅРєР° Р·Р° Р»Р°Р±С‹
 */
 
 #endif
