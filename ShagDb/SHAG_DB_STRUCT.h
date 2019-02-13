a653f3093b3dc94899d916be8cc45ec834c54b8e
KuznetsovAlexandr <45228899+KuznetsovAlexandr@users.noreply.github.com>
2019-02-13 21:06:38 +0300

    Update SHAG_DB_STRUCT.h

--- ShagDb/SHAG_DB_STRUCT.h
+++ ShagDb/SHAG_DB_STRUCT.h
@@ -9,56 +9,56 @@ const int N = 1;
 
 typedef struct Contact
 {
-	string address; // адрес
-	int tel;           // телефон без +7 и без пробелов
+	string address; // Р°РґСЂРµСЃ
+	int tel;           // С‚РµР»РµС„РѕРЅ Р±РµР· +7 Рё Р±РµР· РїСЂРѕР±РµР»РѕРІ
 };
 
 typedef struct People
 {
-	string family;       // фамилия
-	string name;         // имя
-	Contact contact;       // контактная информация
+	string family;       // С„Р°РјРёР»РёСЏ
+	string name;         // РёРјСЏ
+	Contact contact;       // РєРѕРЅС‚Р°РєС‚РЅР°СЏ РёРЅС„РѕСЂРјР°С†РёСЏ
 };
 
 typedef struct Date
 {
-	int day;              // день
-	int month;            // месяц
-	int year;             // год
+	int day;              // РґРµРЅСЊ
+	int month;            // РјРµСЃСЏС†
+	int year;             // РіРѕРґ
 };
 
 typedef struct Visit
 {
-	Date date;            // день, месяц, год
-	int status;           // статус посещения (0 - отсутствовал, 1 - присутствовал, 2 - болел)
+	Date date;            // РґРµРЅСЊ, РјРµСЃСЏС†, РіРѕРґ
+	int status;           // СЃС‚Р°С‚СѓСЃ РїРѕСЃРµС‰РµРЅРёСЏ (0 - РѕС‚СЃСѓС‚СЃС‚РІРѕРІР°Р», 1 - РїСЂРёСЃСѓС‚СЃС‚РІРѕРІР°Р», 2 - Р±РѕР»РµР»)
 };
 
 typedef struct AcadPerf
 {
-	Date date;            // день, месяц, год
-	int ac_status;        // оценка
+	Date date;            // РґРµРЅСЊ, РјРµСЃСЏС†, РіРѕРґ
+	int ac_status;        // РѕС†РµРЅРєР°
 };
 
 typedef struct Facultet
 {
-	string name;           // название факультета
-	string group;            // название группы
-	bool actual;              // актуальность (числится или нет)
+	string name;           // РЅР°Р·РІР°РЅРёРµ С„Р°РєСѓР»СЊС‚РµС‚Р°
+	string group;            // РЅР°Р·РІР°РЅРёРµ РіСЂСѓРїРїС‹
+	bool actual;              // Р°РєС‚СѓР°Р»СЊРЅРѕСЃС‚СЊ (С‡РёСЃР»РёС‚СЃСЏ РёР»Рё РЅРµС‚)
 };
 
 typedef struct Student
 {
-	int id;                   // уникальный идентификатор
-	People people;            // фамилия, имя, контактная информация (адрес и телефон)
-	Visit visit[N];           // посещаемость
+	int id;                   // СѓРЅРёРєР°Р»СЊРЅС‹Р№ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂ
+	People people;            // С„Р°РјРёР»РёСЏ, РёРјСЏ, РєРѕРЅС‚Р°РєС‚РЅР°СЏ РёРЅС„РѕСЂРјР°С†РёСЏ (Р°РґСЂРµСЃ Рё С‚РµР»РµС„РѕРЅ)
+	Visit visit[N];           // РїРѕСЃРµС‰Р°РµРјРѕСЃС‚СЊ
 
-	AcadPerf ac_perf[N];      // текушая успеваемость (оценка)
-	AcadPerf homework[N];     // оценка за домашнюю работу
-	AcadPerf test[N];         // оценка за контрольную работу
-	AcadPerf lab[N];          // оценка за лабораторную
-	float middle_ac;            // средневзвещенная оценка  
+	AcadPerf ac_perf[N];      // С‚РµРєСѓС€Р°СЏ СѓСЃРїРµРІР°РµРјРѕСЃС‚СЊ (РѕС†РµРЅРєР°)
+	AcadPerf homework[N];     // РѕС†РµРЅРєР° Р·Р° РґРѕРјР°С€РЅСЋСЋ СЂР°Р±РѕС‚Сѓ
+	AcadPerf test[N];         // РѕС†РµРЅРєР° Р·Р° РєРѕРЅС‚СЂРѕР»СЊРЅСѓСЋ СЂР°Р±РѕС‚Сѓ
+	AcadPerf lab[N];          // РѕС†РµРЅРєР° Р·Р° Р»Р°Р±РѕСЂР°С‚РѕСЂРЅСѓСЋ
+	float middle_ac;            // СЃСЂРµРґРЅРµРІР·РІРµС‰РµРЅРЅР°СЏ РѕС†РµРЅРєР°  
 
-	Facultet facult;          // факультет
+	Facultet facult;          // С„Р°РєСѓР»СЊС‚РµС‚
 };
 
 #endif 
